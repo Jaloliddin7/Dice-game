a7420019d4bc18d3c9f478403ead9cfa823ea841
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-02-22 08:34:47 +0500

    Mana Ketyapti

--- dice game/script.js
+++ dice game/script.js
@@ -2,6 +2,15 @@ const btnNew = document.querySelector(".btn--new")
 const btnRoll = document.querySelector(".btn--roll")
 const btnHold = document.querySelector(".btn--hold")
 
+const imgDice = document.querySelector('.dice')
+imgDice.style.display = 'none'
+
+
+btnRoll.addEventListener('click', () => {
+    imgDice.style.display = 'block'
+})
+
+
 
 
 
